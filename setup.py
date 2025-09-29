943bea79479bc7ef28608a44e3d4034deb17c548
Piyush74987 <1911piyushmali@gmail.com>
2025-09-29 17:55:13 +0530

    adding project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Piyush Sanjay Mali",
+    author_email="piyushmali2812@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
